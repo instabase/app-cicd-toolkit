d641f01b7013639c303f74d31749ac8c0d82e731
Hannah Roiter <hannah.roiter@instabase.com>
2024-03-19 15:15:54 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,127 @@
-# app-cicd-toolkit
+## Overview
+
+CI/CD solution to migrate a local solution to a target instabase environment.
+
+The solution uses Instabase APIs and CI tooling to promote solutions between environments. Template CI pipelines for GitHub Actions and Azure DevOps are provided and should be updated to suit customer's needs.
+
+### Python Script
+
+The Python `promote_solution.py` script calls Instabase APIs and can execute the following steps when the corresponding flags are passed in:
+
+- `--compile_source_solution`
+  - Compiles solution in source environment in path specified in `SOURCE_SOLUTION_DIR` environment variable.
+- `--publish_source_solution`
+  - Publishes the latest version of the solution in the source environment to Deployed Solutions
+- `--promote_solution_to_target`
+  - Uploads to the solution to the target environment
+- `--publish_target_solution`
+  - Publishes the latest version of the solution in the target environment to Deployed Solutions. Use the `--local` flag to upload code in the git repository
+- `--upload_dependencies`
+  - Uploads and publishes the solution dependencies to the target environment based on dependencies listed in `package.json`
+- `--download_ibsolution`
+  - Downloads the `.ibsolution` file to the local filesystem
+- `--set_github_actions_env_var`
+  - Sets `PACKAGE_VERSION` environment variable to be used in later steps in the GitHub Actions pipleline
+- `--set_azure_devops_env_var`
+  - Sets `PACKAGE_VERSION` environment to be used in later steps in the Azure DevOps pipeline
+- `--local`
+  - To be used alongside above flags. This will promote a solution from the codebase rather than a remote Instabase environment
+- `--remote`
+  - To be used alongside above flags. This will promote a solution from a source Instbase environment
+- `--local_flow`
+  - Performs series of above steps for a local workflow
+- `--remote_flow`
+  - Performs series of above steps for a remote workflow, `--compile_source_solution`, `--set_azure_devops_env_var`, and `--set_github_actions_env_var` are not included in these so need to be used too
+- `--marketplace`
+  - Publishes solution to Marketplace. If not used then Deployed Solutions will be used
+
+
+### GitHub Actions Workflows
+
+The `local-github-actions` automated pipeline runs on successful merge to main, so the main branch of the project should mirror the target environment. The `remote-github-actions-workflow` is manually run.
+
+The pipeline sets environment variables from GitHub secrets and variables and runs the Python script. It then creates a release stores the downloaded `.ibsolution` file as an artifact.
+
+## Setup
+
+### Configure Repo
+
+1. Set the following git secrets in Settings > Secrets and Variables > Actions:
+    - **SOURCE_IB_API_TOKEN** API token for source environment. Ideally both tokens should belong to a service account
+    - **TARGET_IB_API_TOKEN** API token for target environment
+2. Set git variables in the same place:
+    - **SOURCE_IB_HOST** e.g. "https://solution-eng.aws.sandbox.instabase.com"
+    - **SOURCE_WORKING_DIR** Path in source IB environment's filesystem to use for downloading dependencies
+    - **SOURCE_SOLUTION_DIR** Path to the solution directory in the source Instabase environment
+    - **LOCAL_SOLUTION_DIR** Directory in local repository that contains IB solution code, if using a remote flow leave a blank string
+    - **REL_FLOW_PATH** Path to flow relative to Instabase solution e.g. in "path/to/solution/Flow/some_flow.ibflow" where "path/to/solution" is `SOURCE_SOLUTION_DIR` it would be "Flow/some_flow.ibflow"
+    - **SOURCE_COMPILED_SOLUTIONS_PATH** Path to where `.ibsolution` files are generated. This will usually be one level higher than the `SOURCE_SOLUTION_DIR`
+    - **TARGET_IB_HOST** e.g. "https://instabase.com"
+    - **TARGET_IB_PATH** Path in the target IB environment where the solution will get uploaded
+3. Copy `cicd` folder into repository and move one of the `.yml` workflow files into a `.GitHub/workflows` folder
+
+### Local Workflow
+
+1. Develop in IB environment.
+2. Once happy with changes checkout a new git branch and copy the solution directory into codebase.
+3. Make sure this includes a `package.json` file and an `icon.png` at the top level of the solution code, bump the version in `package.json`
+4. Include any dependencies in the `package.json` file in the format: ```"dependencies": {
+    "models": ["model_for_cicd==0.0.1"],
+    "dev_exchange_packages": ["package_for_cicd==0.0.1"]
+  }```
+5. If using Deployed solutions, ensure the `package.json` file includes `owner` and `visibility` fields
+6. Any final changes e.g. linting
+7. Push local branch to git
+8. Pull request to main
+9. On merge the GitHub actions workflow will:
+   1. Run the Python script with `--local_flow` parameters (see above for steps)
+   2. Create a GitHub release with tag equal to the version stored in `package.json`
+   3. Upload the `.ibsolution` file to the release artifacts
+
+### Remote Workflow
+
+1. Develop in IB environment.
+2. If in a development environment, bump up the version in `package.json` and ensure that `package.json` and `icon.png` files are in the top level of the directory
+3. If using Deployed solutions, ensure the `package.json` file includes fields `owner` set to `IB_DEPLOYED` and `visibility` set to `PUBLIC`
+4. Manually run the GitHub actions workflow which will:
+   1. Run the Python script with `--remote_flow` parameters (see above for steps). If needed also pass in the `--compile_solution` parameter (likely to be needed in a DEV -> UAT workflow but not in UAT -> PROD)
+   2. Commit and push the `.ibsolution` file to the repository
+   3. Create a GitHub release with tag equal to the version stored in `package.json`
+   4. Upload the `.ibsolution` file to the release artifacts
+5. This will compile the solution in the source environment, promote it to the target environment, and store the `.ibsolution` file in the GitHub release
+
+### Sample package.json
+
+```{
+  "name": "Form W-2",
+  "version": "0.0.2",
+  "short_description": "Automatically process Form W-2",
+  "long_description": "Extract key fields from Tax Form W-2 in the United States",
+  "authors": [
+    "Instabase"
+  ],
+  "solution_type": "ibflowbin",
+  "accelerator_type": [
+    "solution"
+  ],
+  "industry": [
+    "Financial Services"
+  ],
+  "business_vertical": [
+    "Retail & Consumer Banking"
+  ],
+  "use_case": [
+    "Account Opening"
+  ],
+  "icon_file": "icon.png",
+  "beta": true,
+  "encryption_config": {
+    "encryption_type": "v1"
+  },
+  "dependencies": {
+    "models": ["model_for_cicd==0.0.1"],
+    "dev_exchange_packages": ["package_for_cicd==0.0.1"]
+  },
+  "owner": "IB_DEPLOYED",
+  "visibility": "PUBLIC"
+}```
---
+++ cicd/promote_solution.py
@@ -0,0 +1,218 @@
+import time
+
+import requests
+import logging
+import json
+
+import os
+import shutil
+from urllib.parse import quote
+import argparse
+import re
+
+from ib_helpers import unzip_files, upload_file, read_file_content_from_ib, copy_file_within_ib, \
+  publish_to_marketplace, delete_folder_or_file_from_ib, deploy_solution
+from migration_helpers import download_ibsolution, compile_and_package_ib_solution, \
+  download_dependencies_from_dev_and_upload_to_prod
+
+TARGET_IB_API_TOKEN = os.environ.get('TARGET_IB_API_TOKEN')
+SOURCE_IB_API_TOKEN = os.environ.get('SOURCE_IB_API_TOKEN')
+
+TARGET_IB_HOST = os.environ.get('TARGET_IB_HOST')
+SOURCE_IB_HOST = os.environ.get('SOURCE_IB_HOST')
+TARGET_IB_PATH = os.environ.get('TARGET_IB_PATH')
+SOURCE_WORKING_DIR = os.environ.get('SOURCE_WORKING_DIR')
+SOURCE_SOLUTION_DIR = os.environ.get('SOURCE_SOLUTION_DIR')
+SOURCE_COMPILED_SOLUTIONS_PATH = os.environ.get('SOURCE_COMPILED_SOLUTIONS_PATH')
+LOCAL_SOLUTION_DIR = os.environ.get('LOCAL_SOLUTION_DIR')
+REL_FLOW_PATH = os.environ.get('REL_FLOW_PATH')
+
+TARGET_FILES_API = os.path.join(*[TARGET_IB_HOST, 'api/v2', 'files'])
+SOURCE_FILES_API = os.path.join(*[SOURCE_IB_HOST, 'api/v2', 'files'])
+
+
+def parse_dependencies(package_dependencies):
+  models = {m.split('==')[0].strip(): m.split('==')[1].strip() for m in package_dependencies.get('models', [])}
+  packages = {p.split('==')[0].strip(): p.split('==')[1].strip() for p in
+              package_dependencies.get('dev_exchange_packages', [])}
+  return {**packages, **models}
+
+
+def read_local_package_json(directory_name, package_path=None):
+  package_path = package_path if package_path else f'{directory_name}/package.json'
+  with open(package_path) as fp:
+    package = json.load(fp)
+  return package
+
+
+def upload_zip_to_instabase():
+  shutil.make_archive('solution', 'zip', LOCAL_SOLUTION_DIR)
+
+  path_to_upload = os.path.join(*[TARGET_IB_PATH, LOCAL_SOLUTION_DIR + '.zip'])
+
+  upload_data = open('solution.zip', 'rb')
+  resp = upload_file(TARGET_IB_HOST, TARGET_IB_API_TOKEN, path_to_upload, upload_data)
+  return resp
+
+
+def version_tuple(v):
+  return tuple(map(int, (v.split("."))))
+
+
+def get_latest_ibsolution_path(api_token, files_api, solution_path):
+  headers = {'Authorization': 'Bearer {0}'.format(api_token)}
+  params = {'expect-node-type': 'folder'}
+  url = os.path.join(files_api, quote(solution_path))
+  resp = requests.get(url, headers=headers, params=params, verify=False)
+  # TODO: Check status code
+
+  nodes = json.loads(resp.content)
+  paths = [s['full_path'] for s in nodes['nodes'] if s['full_path'].endswith('.ibsolution')]
+
+  latest_version = '0.0.0'
+  latest_path = ''
+  for path in paths:
+    sol_name = path.split('.ibsoluton')[0].split('/')[-1]
+    start, end = re.search('-[0-9]*.[0-9]*.[0-9]*', sol_name).span()
+    version = sol_name[start + 1:end]
+    if version_tuple(version) > version_tuple(latest_version):
+      latest_version = version
+      latest_path = path
+  return latest_path
+
+
+def read_target_package():
+  # Unzip solution into a temporary folder
+  new_path = os.path.join(*TARGET_IB_PATH.split('/')[:-1], 'temp_solution')
+  path_to_ib_solution = get_latest_ibsolution_path(TARGET_IB_API_TOKEN, TARGET_FILES_API, TARGET_IB_PATH)
+  unzip_files(TARGET_IB_HOST, TARGET_IB_API_TOKEN, path_to_ib_solution,
+              new_path)  # TODO: Does this unzip work on a non-zip file path like ib_solution_path?
+
+  # Add wait for files to unzip
+  time.sleep(5)
+
+  # Read package
+  package_json_path = os.path.join(new_path, 'package.json')
+  read_response = read_file_content_from_ib(TARGET_IB_HOST, TARGET_IB_API_TOKEN, package_json_path, use_clients=False)
+  package_json = json.loads(read_response.content)
+
+  # Delete temporary solution folder
+  delete_folder_or_file_from_ib(new_path, TARGET_IB_HOST, TARGET_IB_API_TOKEN, use_clients=False)
+  return package_json
+
+
+def set_output_version_github(version):
+  env_file = os.getenv('GITHUB_ENV')
+
+  with open(env_file, "a") as myfile:
+    myfile.write(f"PACKAGE_VERSION={version}")
+
+
+def copy_solution_to_working_dir(new_solution_dir):
+  package_path = os.path.join(SOURCE_SOLUTION_DIR, 'package.json')
+  icon_path = os.path.join(SOURCE_SOLUTION_DIR, 'icon.png')
+  flow_path = os.path.join(SOURCE_SOLUTION_DIR, REL_FLOW_PATH)
+  modules_path = os.path.join(SOURCE_SOLUTION_DIR, *REL_FLOW_PATH.split('/')[:-1], 'modules')
+
+  for path in [package_path, icon_path, flow_path, modules_path]:
+    new_path = path.replace(SOURCE_SOLUTION_DIR, new_solution_dir)
+    copy_file_within_ib(SOURCE_IB_HOST, SOURCE_IB_API_TOKEN, path, new_path, use_clients=False)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--promote_solution_to_target', action='store_true')
+  parser.add_argument('--publish_source_solution', action='store_true')
+  parser.add_argument('--publish_target_solution', action='store_true')
+  parser.add_argument('--upload_dependencies', action='store_true')
+  parser.add_argument('--compile_source_solution', action='store_true')
+  parser.add_argument('--set_github_actions_env_var', action='store_true')
+  parser.add_argument('--set_azure_devops_env_var', action='store_true')
+  parser.add_argument('--download_ibsolution', action='store_true')
+  parser.add_argument('--local', action='store_true')
+  parser.add_argument('--remote', dest='local', action='store_false')
+  parser.add_argument('--local_flow', action='store_true')
+  parser.add_argument('--remote_flow', action='store_true')
+  parser.add_argument('--marketplace', action='store_true')
+  parser.set_defaults(local=False)
+  args = parser.parse_args()
+
+  if args.compile_source_solution:
+    new_solution_dir = os.path.join(SOURCE_WORKING_DIR, SOURCE_SOLUTION_DIR.split('/')[-1])
+    copy_solution_to_working_dir(new_solution_dir)
+    time.sleep(3)
+    compile_and_package_ib_solution(SOURCE_IB_HOST, SOURCE_IB_API_TOKEN, new_solution_dir, REL_FLOW_PATH,
+                                    SOURCE_COMPILED_SOLUTIONS_PATH)
+
+  if args.publish_source_solution or args.local_flow or args.remote_flow:
+    source_path = get_latest_ibsolution_path(SOURCE_IB_API_TOKEN, SOURCE_FILES_API, SOURCE_COMPILED_SOLUTIONS_PATH)
+    if args.marketplace:
+      publish_to_marketplace(SOURCE_IB_HOST, SOURCE_IB_API_TOKEN, source_path)
+    else:
+      deploy_solution(SOURCE_IB_HOST, SOURCE_IB_API_TOKEN, source_path)
+
+  if args.promote_solution_to_target or args.local_flow or args.remote_flow:
+    if args.local or args.local_flow:
+      upload_zip_to_instabase()
+
+      # Unzip solution contents
+      zip_path = os.path.join(*[TARGET_IB_PATH, LOCAL_SOLUTION_DIR + '.zip'])
+      unzip_files(TARGET_IB_HOST, TARGET_IB_API_TOKEN, zip_path)
+
+      directory_path = os.path.join(TARGET_IB_PATH, LOCAL_SOLUTION_DIR)
+      time.sleep(3)
+      compile_and_package_ib_solution(TARGET_IB_HOST, TARGET_IB_API_TOKEN, directory_path, REL_FLOW_PATH,
+                                      TARGET_IB_PATH)
+    else:
+      ib_solution_path = get_latest_ibsolution_path(SOURCE_IB_API_TOKEN, SOURCE_FILES_API,
+                                                    SOURCE_COMPILED_SOLUTIONS_PATH)
+      resp = download_ibsolution(SOURCE_IB_HOST, SOURCE_IB_API_TOKEN, ib_solution_path, True)
+      target_path = os.path.join(TARGET_IB_PATH, ib_solution_path.split('/')[-1])
+      upload_path = os.path.join(TARGET_IB_HOST, 'api/v2', 'files', target_path)
+      upload_file(TARGET_IB_HOST, TARGET_IB_API_TOKEN, upload_path, resp.content)
+
+  if args.publish_target_solution or args.local_flow or args.remote_flow:
+    ib_solution_path = get_latest_ibsolution_path(TARGET_IB_API_TOKEN, TARGET_FILES_API, TARGET_IB_PATH)
+    if args.marketplace:
+      publish_to_marketplace(TARGET_IB_HOST, TARGET_IB_API_TOKEN, ib_solution_path)
+    else:
+      deploy_solution(TARGET_IB_HOST, TARGET_IB_API_TOKEN, ib_solution_path)
+
+  if args.upload_dependencies or args.local_flow or args.remote_flow:
+    if args.local:
+      dependencies = read_local_package_json(LOCAL_SOLUTION_DIR)
+      requirements_dict = parse_dependencies(dependencies.get('dependencies', {}))
+    else:
+      package = read_target_package()
+      requirements_dict = parse_dependencies(package.get('dependencies', {}))
+
+    # Download dependencies needed for ibsolution and upload them onto target environment
+    uploaded_ibsolutions = download_dependencies_from_dev_and_upload_to_prod(SOURCE_IB_HOST, TARGET_IB_HOST,
+                                                                             SOURCE_IB_API_TOKEN, TARGET_IB_API_TOKEN,
+                                                                             SOURCE_WORKING_DIR, TARGET_IB_PATH,
+                                                                             requirements_dict)
+
+    # Publish uploaded ibsolution files to target environment marketplace
+    for ib_solution_path in uploaded_ibsolutions:
+      publish_resp = publish_to_marketplace(TARGET_IB_HOST, TARGET_IB_API_TOKEN, ib_solution_path)
+      logging.info('Publish response for {}: {}'.format(ib_solution_path, publish_resp))
+
+  if args.download_ibsolution or args.local_flow or args.remote_flow:
+    ib_solution_path = get_latest_ibsolution_path(TARGET_IB_API_TOKEN, TARGET_FILES_API, TARGET_IB_PATH)
+    download_ibsolution(TARGET_IB_HOST, TARGET_IB_API_TOKEN, ib_solution_path)
+
+  if args.set_github_actions_env_var:
+    if args.local:
+      package = read_local_package_json(LOCAL_SOLUTION_DIR)
+    else:
+      package = read_target_package()
+    version = package['version']
+    set_output_version_github(version)
+
+  if args.set_azure_devops_env_var:
+    if args.local:
+      package = read_local_package_json(LOCAL_SOLUTION_DIR)
+    else:
+      package = read_target_package()
+    version = package['version']
+    print(f'##vso[task.setvariable variable=PACKAGE_VERSION;]{version}')
