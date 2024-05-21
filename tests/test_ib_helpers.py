02f8a0f7d7e6a276f4f76cbe2dc8ce87f1b51733
Hannah Roiter <hannah.roiter@instabase.com>
2024-05-21 17:04:49 +0100

    Add unit test

--- tests/test_ib_helpers.py
+++ tests/test_ib_helpers.py
@@ -1,14 +1,35 @@
 """Collection of unit tests for IB Helpers"""
 
+import json
 from unittest import mock
 from requests.models import Response
-from ib_cicd.ib_helpers import upload_file
+from ib_cicd.ib_helpers import upload_file, compile_solution
 from tests.fixtures import (
     ib_host_url,
     ib_api_token,
     _MOCK_IB_HOST_URL,
     _MOCK_AUTH_HEADERS,
 )
+
+
+@mock.patch("ib_cicd.ib_helpers.requests")
+def test_compile_solution(mock_requests, ib_host_url, ib_api_token):
+    # Arrange
+    mocked_response = mock.Mock(spec=Response)
+    mocked_response.status_code = 200
+    mocked_response.content = json.dumps({"status": "OK"})
+    mock_requests.post.return_value = mocked_response
+
+    solution_path = "Test Space/Test Subspace/fs/Instabase Drive/My Solution"
+    relative_flow_path = "Path to flow/flow.ibflow"
+    compile_solution(ib_host_url, ib_api_token, solution_path, relative_flow_path)
+
+    mock_requests.post.assert_called_with(
+        "https://instbase-fake-testing-url.com/api/v1/flow_binary/compile/Test%20Space/Test%20Subspace/fs/Instabase%20Drive/My%20Solution",
+        headers={"Authorization": "Bearer fake-testing-token"},
+        data='{"binary_type": "Single Flow", "flow_project_root": "Test Space/Test Subspace/fs/Instabase Drive/My Solution/Path to flow", "predefined_binary_path": "Test Space/Test Subspace/fs/Instabase Drive/My Solution/Path to flow/flow.ibflowbin", "settings": {"flow_file": "flow.ibflow", "is_flow_v3": true}}',
+        verify=False,
+    )
 
 
 @mock.patch("ib_cicd.ib_helpers.requests")
