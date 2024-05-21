95f6a2e1af32a81b6deeb7f8b50398cb4a6bb055
Hannah Roiter <hannah.roiter@instabase.com>
2024-05-21 16:31:39 +0100

    Add flow v3 parameter

--- ib_cicd/ib_helpers.py
+++ ib_cicd/ib_helpers.py
@@ -224,7 +224,10 @@ def compile_solution(ib_host, api_token, solution_path, relative_flow_path):
                 solution_path, *relative_flow_path.split("/")[:-1]
             ),
             "predefined_binary_path": os.path.join(solution_path, bin_path),
-            "settings": {"flow_file": relative_flow_path.split("/")[-1]},
+            "settings": {
+                "flow_file": relative_flow_path.split("/")[-1],
+                "is_flow_v3": True
+            },
         }
     )
     resp = requests.post(
